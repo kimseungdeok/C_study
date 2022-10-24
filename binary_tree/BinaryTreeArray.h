c5e767dc82b810394350ca584b26605b9e58f855
kimseungdeok <tmdejr1117@gmail.com>
2022-10-24 10:25:43 +0900

    solved : 이진 트리 학습 완료

---
+++ binary_tree/BinaryTreeArray.h
@@ -0,0 +1,25 @@
+#ifndef _BINARYTREE_H_
+#define _BINARYTREE_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <memory.h>
+
+// 노드의 데이터타입
+typedef char DATA;
+
+// 노드 : tree[ ]의 index로써 노드를 가리킴.
+typedef int Node;
+
+Node makeRoot(DATA data);
+Node makeLeftChild(Node cur, DATA data);
+Node makeRightChild(Node cur, DATA data);
+
+DATA getCurData(Node cur);
+Node getLeftChild(Node cur);
+DATA getLeftChildData(Node Cur);
+Node getRightChild(Node cur);
+DATA getRightChildData(Node cur);
+int isTreeEmpty(Node root);
+
+#endif // _BINARYTREE_H_

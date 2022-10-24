75481361ab41c06a68b4dcc8d788f4a11decd325
kimseungdeok <tmdejr1117@gmail.com>
2022-10-24 11:14:52 +0900

    solvedd : 연결 리스트 구현 완료

---
+++ binary_tree/linked_list/BinaryTreeList.c
@@ -0,0 +1,48 @@
+#include "BinaryTreeList.h"
+
+// 트리
+Node tree = NULL;
+
+
+Node makeRoot(DATA data){
+    tree = (struct _Node*)malloc(sizeof(struct _Node));
+    tree->data = data;
+    tree->lchild = tree->rchild = NULL;
+    return tree;
+}
+Node makeLeftChild(Node cur, DATA data){
+    struct _Node* newnode = (struct _Node*)malloc(sizeof(struct _Node));
+    newnode->data = data;
+    newnode->lchild = newnode->rchild = NULL;
+    cur->lchild = newnode;
+    return newnode;
+}
+Node makeRightChild(Node cur, DATA data){
+    struct _Node* newnode = (struct _Node*)malloc(sizeof(struct _Node));
+    newnode->data = data;
+    newnode->lchild = newnode->rchild = NULL;
+    cur->rchild = newnode;
+    return newnode;
+}
+
+DATA getCurData(Node cur){
+    return cur->data;
+}
+Node getLeftChild(Node cur){
+    return cur->lchild;
+}
+DATA getLeftChildData(Node cur){
+    return cur->lchild->data;
+}
+Node getRightChild(Node cur){
+    return cur->rchild;
+}
+DATA getRightChildData(Node cur){
+    return cur->rchild->data;
+}
+int isTreeEmpty(Node root){
+    if (root==NULL)
+        return 1;
+    else
+        return 0;
+}
---
+++ binary_tree/linked_list/main.c
@@ -0,0 +1,45 @@
+#include "BinaryTreeList.h"
+
+void preorder(Node root)
+{
+    if (isTreeEmpty(root))
+        return;
+    printf("%c", getCurData(root));
+    preorder(getLeftChild(root));
+    preorder(getRightChild(root));
+}
+
+void inorder(Node root)
+{
+    if (isTreeEmpty(root))
+        return;
+    inorder(getLeftChild(root));
+    printf("%c", getCurData(root));
+    inorder(getRightChild(root));
+}
+
+void postorder(Node root)
+{
+    if (isTreeEmpty(root))
+        return;
+    postorder(getLeftChild(root));
+    postorder(getRightChild(root));
+    printf("%c", getCurData(root));
+}
+
+void main(){
+    Node _a = makeRoot('A');
+    Node _b = makeLeftChild(_a, 'B');
+    Node _c = makeRightChild(_a, 'C');
+    Node _d = makeLeftChild(_b, 'D');
+    Node _e = makeRightChild(_b, 'E');
+    Node _f = makeLeftChild(_c, 'F');
+    Node _g = makeRightChild(_c, 'G');
+
+    preorder(_a);
+    printf("\n");
+    inorder(_a);
+    printf("\n");
+    postorder(_a);
+    printf("\n");
+}
